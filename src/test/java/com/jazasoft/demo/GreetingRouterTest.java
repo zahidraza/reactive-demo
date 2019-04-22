2bb691213ebdad9b424e33efebd4e94467ee38ba
Md Zahid Raza <zahid7292@ymail.com>
2019-04-22 23:16:34 +0530

    hello implementation using router functions

---
+++ src/test/java/com/jazasoft/demo/GreetingRouterTest.java
@@ -0,0 +1,29 @@
+package com.jazasoft.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingRouterTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void testHello() {
+        webTestClient
+                // Create a GET request to test an endpoint
+                .get().uri("/hello")
+                .accept(MediaType.TEXT_PLAIN)
+                .exchange()
+                // and use the dedicated DSL to test assertions against the response
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("Hello, Spring!");
+    }
+}
